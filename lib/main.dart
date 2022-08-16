b69356e6e523e7605e2381e138694686cbb5701e
shasan11 <shasandkl4gmail.comgit config --global user.email shasandkl4gmail.com>
2022-08-16 12:51:41 +0545

    stateful widgit

--- lib/main.dart
+++ lib/main.dart
@@ -6,25 +6,37 @@ void main() {
   runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({Key? key}) : super(key: key);
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.green,
-              title: const Text("Productivity Clock"),
-            ),
-            body: ListView.builder(itemBuilder: (_, index) {
-              return Container(
-                //its infite
-                color:
-                    Colors.primaries[Random().nextInt(Colors.primaries.length)],
-                width: 50,
-                height: 400,
-              );
-            })));
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text("Productivity Clock"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (() {
+          setState(() {
+            count++; //to manage state use provider package
+          });
+        }),
+      ),
+      body: Center(
+        child: Text(
+          '$count',
+          style: TextStyle(fontSize: 100),
+        ),
+      ),
+    ));
   }
 }
